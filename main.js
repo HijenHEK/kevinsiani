e61c7c866ba03ec44c181d65607caa6995f15c71
HijenHek <k.hijen@gmail.com>
2020-10-09 08:19:53 +0100

    resp tweaks

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ $(document).ready(() => {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 800);
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-
+       
     });
     
     $(document).on('click', '#humburger', function (event) {
@@ -16,6 +14,29 @@ $(document).ready(() => {
         $('.nav-bar .nav').toggleClass('absolute');
 
     });
+
+    $(window).scroll(function () {
+      
+        
+        var top = $(window).scrollTop()+400;
+        var intro = $('#intro').offset().top;
+        var workout = $('#workout').offset().top;
+        var contact = $('#contact').offset().top;
+        var el = $('a[href="#intro"]');
+        if(top>=contact){
+            el = $('a[href="#contact"]');
+        }else if (top >=workout) {
+            el = $('a[href="#workout"]');
+
+        }else if (top >= intro) {
+            el = $('a[href="#intro"]');
+        }
+        el.siblings().removeClass('active');
+        el.addClass('active');
+
+
+    });
+
 });
 
 
